97b875a55b346824c1c0366c75abd6697c51ca1e
Linda-Boateng <linda.boateng@amalitech.com>
2024-05-09 10:13:23 +0000

    refactored

--- src/test/java/com/example/weektwotesting/OrderServiceImplIntegrationTest.java
+++ src/test/java/com/example/weektwotesting/OrderServiceImplIntegrationTest.java
@@ -3,22 +3,17 @@ package com.example.weektwotesting;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
 @SpringJUnitConfig
 @WebMvcTest(OrderController.class)
 @AutoConfigureMockMvc
@@ -31,7 +26,7 @@ class OrderServiceImplIntegrationTest {
 
   @Test
   void testAddOrder() throws Exception {
-        // Arrange
+
         Order order = new Order();
         order.setId(1L);
         order.setProductName("Test Product");
@@ -39,7 +34,6 @@ class OrderServiceImplIntegrationTest {
 
         Mockito.doNothing().when(orderService).saveOrder(Mockito.any(Order.class));
 
-        // Act & Assert
         mockMvc.perform(MockMvcRequestBuilders.post("/order")
                         .content(asJsonString(order))
                         .contentType(MediaType.APPLICATION_JSON)
@@ -47,7 +41,6 @@ class OrderServiceImplIntegrationTest {
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
-    // Helper method to convert object to JSON string
     private static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
